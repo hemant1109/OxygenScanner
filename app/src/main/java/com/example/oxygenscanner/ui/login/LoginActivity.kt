fdaaeb46c4fad15a583e298de6d561b65845ea64
Parvin Pathan <pgpathan22@gmail.com>
2021-06-05 17:30:28 +0530

    Login firebase completed.

--- app/src/main/java/com/example/oxygenscanner/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/oxygenscanner/ui/login/LoginActivity.kt
@@ -1,139 +1,157 @@
 package com.example.oxygenscanner.ui.login
 
-import android.app.Activity
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
+import android.util.Log
 import android.view.View
-import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import android.widget.Toast
-import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.oxygenscanner.R
 import com.example.oxygenscanner.databinding.ActivityLoginBinding
 import com.example.oxygenscanner.ui.startvitalsign.StartVitalSigns
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
 
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var username: EditText
+    private lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
+    private lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
+    private lateinit var storedVerificationId: String
+    private lateinit var edtMobileNumber: EditText
     private lateinit var loginViewModel: LoginViewModel
     private lateinit var binding: ActivityLoginBinding
+    lateinit var auth:FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
+        FirebaseApp.initializeApp(this)
+        auth = FirebaseAuth.getInstance()
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        username = binding.username
-        val password = binding.password
-        val login = binding.login
+        edtMobileNumber = binding.mobileNumber
+        val edtOtp = binding.otp
+        val btnSendAndVerifyOtp = binding.sendAndVerifyOtp
         val loading = binding.loading
 
         loginViewModel = ViewModelProvider(this, LoginViewModelFactory())
             .get(LoginViewModel::class.java)
 
-        loginViewModel.loginFormState.observe(this@LoginActivity, Observer {
-            val loginState = it ?: return@Observer
 
-            // disable login button unless both username / password is valid
-            login.isEnabled = loginState.isDataValid
-
-            if (loginState.usernameError != null) {
-                username.error = getString(loginState.usernameError)
+        btnSendAndVerifyOtp.setOnClickListener {
+            if (btnSendAndVerifyOtp.text == "Send Otp") {
+                loginViewModel.sendOtp(this, edtMobileNumber.text.toString(), callbacks)
+            }else{
+                val credential = PhoneAuthProvider.getCredential(storedVerificationId!!, edtOtp.text.toString())
+                signInWithPhoneAuthCredential(credential)
+            ///loginViewModel.verifyOtp( this@LoginActivity,edtMobileNumber.text.toString(),edtOtp.text.toString())
             }
-            if (loginState.passwordError != null) {
-                password.error = getString(loginState.passwordError)
-            }
-        })
+        }
 
-        loginViewModel.loginResult.observe(this@LoginActivity, Observer {
+        loginViewModel.otpSend.observe(this@LoginActivity, Observer {
             val loginResult = it ?: return@Observer
-
-            loading.visibility = View.GONE
-            if (loginResult.error != null) {
-                showLoginFailed(loginResult.error)
+            if (loginResult) {
+                loading.visibility = View.GONE
+                btnSendAndVerifyOtp.text = "Verify Otp"
+                edtOtp.visibility = View.VISIBLE
+            } else {
+                showLoginFailed("Please try agian later")
             }
-            if (loginResult.success != null) {
-                updateUiWithUser(loginResult.success)
-            }
-            setResult(Activity.RESULT_OK)
 
-            startVitalSignActivity()
         })
 
-        username.afterTextChanged {
-            loginViewModel.loginDataChanged(
-                username.text.toString(),
-                password.text.toString()
-            )
-        }
+        callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
 
-        password.apply {
-            afterTextChanged {
-                loginViewModel.loginDataChanged(
-                    username.text.toString(),
-                    password.text.toString()
-                )
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                // This callback will be invoked in two situations:
+                // 1 - Instant verification. In some cases the phone number can be instantly
+                //     verified without needing to send or enter a verification code.
+                // 2 - Auto-retrieval. On some devices Google Play services can automatically
+                //     detect the incoming verification SMS and perform verification without
+                //     user action.
+                Log.d(TAG, "onVerificationCompleted:$credential")
+
             }
 
-            setOnEditorActionListener { _, actionId, _ ->
-                when (actionId) {
-                    EditorInfo.IME_ACTION_DONE ->
-                        loginViewModel.login(
-                            username.text.toString(),
-                            password.text.toString()
-                        )
+            override fun onVerificationFailed(e: FirebaseException) {
+                // This callback is invoked in an invalid request for verification is made,
+                // for instance if the the phone number format is not valid.
+                Log.w(TAG, "onVerificationFailed", e)
+
+                if (e is FirebaseAuthInvalidCredentialsException) {
+                    // Invalid request
+                } else if (e is FirebaseTooManyRequestsException) {
+                    // The SMS quota for the project has been exceeded
                 }
-                false
+
+                // Show a message and update the UI
             }
 
-            login.setOnClickListener {
-                loading.visibility = View.VISIBLE
-                loginViewModel.login(username.text.toString(), password.text.toString())
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken
+            ) {
+                // The SMS verification code has been sent to the provided phone number, we
+                // now need to ask the user to enter the code and then construct a credential
+                // by combining the code with a verification ID.
+                Log.d(TAG, "onCodeSent:$verificationId")
+                Toast.makeText(this@LoginActivity, "Otp send successfully", Toast.LENGTH_SHORT)
+                    .show()
+                loading.visibility = View.GONE
+                btnSendAndVerifyOtp.text = "Verify Otp"
+                edtOtp.visibility = View.VISIBLE
+                // Save verification ID and resending token so we can use them later
+                storedVerificationId = verificationId
+                resendToken = token
             }
         }
+
+
+
     }
 
     private fun startVitalSignActivity() {
         val i = Intent(this, StartVitalSigns::class.java)
-        i.putExtra("Usr", username.text.toString())
+        i.putExtra("Usr", edtMobileNumber.text.toString())
         startActivity(i)
         finish()
     }
 
-    private fun updateUiWithUser(model: LoggedInUserView) {
-        val welcome = getString(R.string.welcome)
-        val displayName = model.displayName
-        // TODO : initiate successful logged in experience
-        Toast.makeText(
-            applicationContext,
-            "$welcome $displayName",
-            Toast.LENGTH_LONG
-        ).show()
-    }
-
-    private fun showLoginFailed(@StringRes errorString: Int) {
+    private fun showLoginFailed(errorString: String) {
         Toast.makeText(applicationContext, errorString, Toast.LENGTH_SHORT).show()
     }
-}
 
-/**
- * Extension function to simplify setting an afterTextChanged action to EditText components.
- */
-fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
-    this.addTextChangedListener(object : TextWatcher {
-        override fun afterTextChanged(editable: Editable?) {
-            afterTextChanged.invoke(editable.toString())
-        }
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    Toast.makeText(applicationContext, "Login In", Toast.LENGTH_SHORT).show()
+                    val user = task.result?.user
+                    startVitalSignActivity()
+                } else {
+                    // Sign in failed, display a message and update the UI
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                        // The verification code entered was invalid
+                        Toast.makeText(applicationContext, "Code is invalid", Toast.LENGTH_SHORT).show()
 
-        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+                    }
+                    // Update UI
+                }
+            }
+    }
 
-        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
-    })
 }
